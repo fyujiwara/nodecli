bcfe2e359ccd03995089d4086bebcc440cbf229c
yujiloh0816 <imp.yfujiwara@gmail.com>
2021-01-03 00:25:50 +0900

    ファイルをUTF-8として非同期で読み込む

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ program.parse(process.argv);
 const filePath = program.args[0];
 console.log(filePath);
 
-// ファイルを非同期で読み込む
-fs.readFile(filePath, (err, file) => {
+// ファイルをUTF-8として非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     console.log(file);
 });
