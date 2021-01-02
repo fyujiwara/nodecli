c04d79d11df0e96cb18135eb24ee1c8d150e4390
yujiloh0816 <imp.yfujiwara@gmail.com>
2021-01-03 00:24:18 +0900

    ファイルを非同期で読み込む

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 // commanderモジュールをprogramとしてインポートする
 const program = require("commander");
+// fsモジュールをfsオブジェクトとしてインポートする
+const fs = require("fs");
+
 // コマンドライン引数をcommanderでパースする
 program.parse(process.argv);
 
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
 console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, (err, file) => {
+    console.log(file);
+});
