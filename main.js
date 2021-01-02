fefb60343d2b4cf8c744563e9f87dcc40d7cda8d
yujiloh0816 <imp.yfujiwara@gmail.com>
2021-01-03 00:27:13 +0900

    ファイル読み込みのエラーハンドリング

--- main.js
+++ main.js
@@ -12,5 +12,11 @@ console.log(filePath);
 
 // ファイルをUTF-8として非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+        process.exit(1);
+        return;
+    }
     console.log(file);
 });
